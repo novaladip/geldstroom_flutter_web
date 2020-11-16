fed6d5455f0e7481efaff2f29087efd4fdd5520c
A. Noval Adip <a.novaladip@yahoo.com>
2020-11-16 17:41:36 +0700

    added assets

---
+++ lib/constant/assets.dart
@@ -0,0 +1,7 @@
+class Assets {
+  static const baseIcons = '/assets/images/icons';
+  static const welcoming = '$baseIcons/welcoming.svg';
+  static const visualData = '$baseIcons/visual_data.svg';
+  static const server = '$baseIcons/server_status.svg';
+  static const beer = '$baseIcons/beer.svg';
+}
